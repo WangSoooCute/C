5322aef8c3fa6d75e254f2e34127243f66117a40
WangSoooCute <38801071+WangSoooCute@users.noreply.github.com>
2023-03-03 18:55:33 +0800

    Add files via upload

---
+++ 用map的find函数来查找.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<map>
+using namespace std;
+//map的底层是一个二叉搜索树
+
+int main(){
+    map<int,int>findIndex;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        findIndex[x]=i;
+        //即把数组的元素和下标反过来
+    }
+    int x;
+    cin>>x;
+
+    if(findIndex.find(x)==findIndex.end())
+        cout<<"NO!"<<endl;
+    else{
+        cout<<"yes!"<<endl;
+        cout<<findIndex[x]+1<<endl;
+    }
+    return 0;
+}
